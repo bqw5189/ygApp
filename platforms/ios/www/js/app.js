1322d75f24094dc760f45592dbe59a9de5e4ea0d
bqw_5189 <bqw_5189@163.com>
2015-03-13 09:35:46 +0800

    ios build

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -151,6 +151,42 @@ angular.module('field', ['ionic', 'field.controllers','tabs.controllers','css.co
 	      }
 	    }
 	  })
+  .state('app.cards', {
+	    url: "/cards",
+	    views: {
+	      'menuContent': {
+	        templateUrl: "modules/components/css/cards.html",
+	        controller:'CardsCtrl'
+	      }
+	    }
+	  })
+ .state('app.card', {
+	    url: "/cards/:listTitle",
+	    views: {
+	      'menuContent': {
+	        templateUrl: "modules/components/css/card.html",
+	        controller:'CardCtrl'
+	      }
+	    }
+	  })
+  .state('app.forms', {
+	    url: "/forms",
+	    views: {
+	      'menuContent': {
+	        templateUrl: "modules/components/css/forms.html",
+	        controller:'FormsCtrl'
+	      }
+	    }
+	  })
+ .state('app.form', {
+	    url: "/forms/:listTitle",
+	    views: {
+	      'menuContent': {
+	        templateUrl: "modules/components/css/form.html",
+	        controller:'FormCtrl'
+	      }
+	    }
+	  })
 	  ;
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/tabs/chats');
--- platforms/ios/www/modules/components/css.js
+++ platforms/ios/www/modules/components/css.js
@@ -33,7 +33,47 @@ angular.module('css.controllers', [])
 	                    { title: 'Cowbell', id: 6 ,img:'img/avatar-6.jpg',lastMessage:'明天见！'}
 	                  ];
 })
-
+.controller('CardsCtrl', function($scope) {
+	$scope.lists = [
+	                    { title: 'Cards', id: 'Cards' },
+	                    { title: 'Card Headers and Footers', id: 'CardHF' },
+	                    { title: 'Card Lists', id: 'CardLists' },
+	                    { title: 'Card Images', id: 'CardImages' },
+	                    { title: 'Card Showcase', id: 'CardShowcase' },
+	                  ];
+})
+.controller('CardCtrl', function($scope, $stateParams) {
+	$scope.listTitle =  $stateParams.listTitle;;
+	$scope.chatlists = [
+	                    { title: 'Reggae', id: 1 ,img:'img/avatar-1.jpg',lastMessage:'你好！'},
+	                    { title: 'Chill', id: 2 ,img:'img/avatar-2.jpg',lastMessage:'谢谢！'},
+	                    { title: 'Dubstep', id: 3,img:'img/avatar-3.jpg' ,lastMessage:'下午开会！'},
+	                    { title: 'Indie', id: 4 ,img:'img/avatar-4.jpg',lastMessage:'汇报下工作！'},
+	                    { title: 'Rap', id: 5 ,img:'img/avatar-5.jpg',lastMessage:'工作已经完成！'},
+	                    { title: 'Cowbell', id: 6 ,img:'img/avatar-6.jpg',lastMessage:'明天见！'}
+	                  ];
+})
+.controller('FormsCtrl', function($scope) {
+	$scope.lists = [
+	                    { title: 'Text Input: Placeholder Labels', id: 'PlaceholderLabels' },
+	                    { title: 'Text Input: Inline Labels', id: 'InlineLabels' },
+	                    { title: 'Text Input: Stacked Labels', id: 'StackedLabels' },
+	                    { title: 'Text Input: Floating Labels', id: 'FloatingLabels' },
+	                    { title: 'Inset Forms', id: 'InsetForms' },
+	                    { title: 'Inset Inputs', id: 'InsetInputs' },
+	                  ];
+})
+.controller('FormCtrl', function($scope, $stateParams) {
+	$scope.listTitle =  $stateParams.listTitle;;
+	$scope.chatlists = [
+	                    { title: 'Reggae', id: 1 ,img:'img/avatar-1.jpg',lastMessage:'你好！'},
+	                    { title: 'Chill', id: 2 ,img:'img/avatar-2.jpg',lastMessage:'谢谢！'},
+	                    { title: 'Dubstep', id: 3,img:'img/avatar-3.jpg' ,lastMessage:'下午开会！'},
+	                    { title: 'Indie', id: 4 ,img:'img/avatar-4.jpg',lastMessage:'汇报下工作！'},
+	                    { title: 'Rap', id: 5 ,img:'img/avatar-5.jpg',lastMessage:'工作已经完成！'},
+	                    { title: 'Cowbell', id: 6 ,img:'img/avatar-6.jpg',lastMessage:'明天见！'}
+	                  ];
+})
 
 ;
 
