344fc8344d59544e5f71f46c04706678be1ed480
bqw_5189 <bqw_5189@163.com>
2015-03-13 14:33:01 +0800

    add tab

--- www/modules/components/css.js
+++ www/modules/components/css.js
@@ -75,6 +75,26 @@ angular.module('css.controllers', [])
 	                    { title: 'Cowbell', id: 6 ,img:'img/avatar-6.jpg',lastMessage:'明天见！'}
 	                  ];
 })
+.controller('TabsCtrl', function($scope) {
+	$scope.lists = [
+	                    { title: 'Tabs', id: 'Tabs' },
+	                    { title: 'Icon-only Tabs', id: 'Icon-only' },
+	                    { title: 'Top Icon Tabs', id: 'Top-Icon' },
+	                    { title: 'Left Icon Tabs', id: 'Left-Icon' },
+	                    { title: 'Striped Style Tabs', id: 'Striped-Style' }
+	                  ];
+})
+.controller('TabCtrl', function($scope, $stateParams) {
+	$scope.listTitle =  $stateParams.listTitle;;
+	$scope.chatlists = [
+	                    { title: 'Reggae', id: 1 ,img:'img/avatar-1.jpg',lastMessage:'你好！'},
+	                    { title: 'Chill', id: 2 ,img:'img/avatar-2.jpg',lastMessage:'谢谢！'},
+	                    { title: 'Dubstep', id: 3,img:'img/avatar-3.jpg' ,lastMessage:'下午开会！'},
+	                    { title: 'Indie', id: 4 ,img:'img/avatar-4.jpg',lastMessage:'汇报下工作！'},
+	                    { title: 'Rap', id: 5 ,img:'img/avatar-5.jpg',lastMessage:'工作已经完成！'},
+	                    { title: 'Cowbell', id: 6 ,img:'img/avatar-6.jpg',lastMessage:'明天见！'}
+	                  ];
+})
 
 ;
 
